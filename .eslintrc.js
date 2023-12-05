2a78ada225b3aed8975c96eb1c17d06b8ba17510
Jean Desauw <desauwjean@gmail.com>
2023-12-05 11:21:31 +0100

    Project Config > Header > Footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,15 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'sort-imports': 'off',
+    'simple-import-sort/imports': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'no-console': 'off',
+  },
 }
